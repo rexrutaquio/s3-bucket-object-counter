f54e8bce3fd67e87d1c990022da8b3ff510dda2a
rexrutaquio <75057800+rexrutaquio@users.noreply.github.com>
2025-05-25 11:40:59 +1000

    Create s3Objects-counter.py

---
+++ s3Objects-counter.py
@@ -0,0 +1,37 @@
+import boto3
+import os
+
+# Initialize S3 and CloudWatch clients
+s3 = boto3.client('s3')
+cloudwatch = boto3.client('cloudwatch')
+
+def lambda_handler(event, context):
+    bucket_name = os.environ['BUCKET_NAME']
+    
+    # List objects in the S3 bucket
+    response = s3.list_objects_v2(Bucket=bucket_name)
+    
+    # Count the number of objects
+    object_count = response.get('KeyCount', 0)
+    
+    # Publish the count to CloudWatch
+    cloudwatch.put_metric_data(
+        Namespace='S3ObjectCount',
+        MetricData=[
+            {
+                'MetricName': 'ObjectCount',
+                'Dimensions': [
+                    {
+                        'Name': 'BucketName',
+                        'Value': bucket_name
+                    },
+                ],
+                'Value': object_count,
+                'Unit': 'Count'
+            },
+        ]
+    )
+    return {
+        'statusCode': 200,
+        'body': f'{object_count} objects in bucket {bucket_name}.'
+    }
